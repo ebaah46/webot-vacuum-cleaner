5750badb5b72a5fe818df7a017a3f193bae2d45e
ebaah <ebaah46@yahoo.com>
2022-10-07 12:53:17 +0300

    update to controller

--- controllers/create_avoid_obstacles/create_avoid_obstacles.cpp
+++ controllers/create_avoid_obstacles/create_avoid_obstacles.cpp
@@ -1,7 +1,7 @@
 // File:          create_avoid_obstacles.cpp
 // Date:
-// Description:
-// Author:
+// Description:   webots controller for irobot
+// Author:        Emmanuel Baah
 // Modifications:
 
 // You may need to add webots include files such as
@@ -13,27 +13,16 @@
 
 #define TIME_STEP 64
 #define MAX_SPEED 6.28
-// All the webots classes are defined in the "webots" namespace
+//Use webots namespace for all classes
 using namespace webots;
 
-// This is the main program of your controller.
-// It creates an instance of your Robot instance, launches its
-// function(s) and destroys it at the end of the execution.
-// Note that only one instance of Robot should be created in
-// a controller program.
-// The arguments of the main function can be specified by the
-// "controllerArgs" field of the Robot node
 int main(int argc, char **argv) {
   // create the Robot instance.
   Robot *robot = new Robot();
 
   // get the time step of the current world.
   int timeStep = (int)robot->getBasicTimeStep();
-  // You should insert a getDevice-like function in order to get the
-  // instance of a device of the robot. Something like:
-  //  Motor *motor = robot->getMotor("motorname");
-  //  DistanceSensor *ds = robot->getDistanceSensor("dsname");
-  //  ds->enable(timeStep);
+  // instance of a device of the robot.
     DistanceSensor *ps[8];
     char psNames[8][4] = {
             "ps0","ps1","ps2","ps3",
@@ -52,9 +41,7 @@ int main(int argc, char **argv) {
   leftMotor->setVelocity(0.0);
   rightMotor->setVelocity(0.0);
 
- //
 
-  // Main loop:
   // - perform simulation steps until Webots is stopping the controller
   while (robot->step(timeStep) != -1) {
     // Read the sensors:
@@ -83,13 +70,12 @@ int main(int argc, char **argv) {
           leftSpeed = -0.5 * MAX_SPEED;
           rightSpeed = 0.5 * MAX_SPEED;
       }
-    // Enter here functions to send actuator commands, like:
-    //  motor->setPosition(10.0);
+    // Set actuator commands
     leftMotor->setVelocity(leftSpeed);
     rightMotor->setVelocity(rightSpeed);
   };
 
-  // Enter here exit cleanup code.
+  //  cleanup code.
 
   delete robot;
   return 0;
